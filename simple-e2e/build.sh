76c5f95f2cdde7e8cee9c4da32a4f82ecb2a2c9c
alan bennett <alan@opensourcefoundries.com>
2018-04-06 13:30:53 -0600

    add docker-compose

--- simple-e2e/build.sh
+++ simple-e2e/build.sh
@@ -7,50 +7,56 @@ arch=""
 [ `arch` == armhf ] && arch="-arm"
 [ `arch` == x86_64 ] && arch="-amd64"
 
+# create_and_push_manifest
+function create_and_push_manifest {
+    ACCOUNT=$1
+    D=$2
+    # create a manifest for atleast 1 image
+    docker manifest create --amend \
+        ${ACCOUNT:-opensourcefoundries}/$D:latest \
+            ${ACCOUNT:-opensourcefoundries}/$D:latest$arch
+
+    # create a manifest for atleast 2 images
+    docker manifest create --amend \
+        ${ACCOUNT:-opensourcefoundries}/$D:latest \
+            ${ACCOUNT:-opensourcefoundries}/$D:latest-arm64 \
+            ${ACCOUNT:-opensourcefoundries}/$D:latest-amd64
+    docker manifest create --amend \
+        ${ACCOUNT:-opensourcefoundries}/$D:latest \
+            ${ACCOUNT:-opensourcefoundries}/$D:latest-arm64 \
+            ${ACCOUNT:-opensourcefoundries}/$D:latest-arm
+    docker manifest create --amend \
+        ${ACCOUNT:-opensourcefoundries}/$D:latest \
+            ${ACCOUNT:-opensourcefoundries}/$D:latest-arm64 \
+            ${ACCOUNT:-opensourcefoundries}/$D:latest-amd64
+
+    # create a manifest for atleast 2 images
+    docker manifest create --amend \
+        ${ACCOUNT:-opensourcefoundries}/$D:latest \
+            ${ACCOUNT:-opensourcefoundries}/$D:latest-arm64 \
+            ${ACCOUNT:-opensourcefoundries}/$D:latest-amd64 \
+            ${ACCOUNT:-opensourcefoundries}/$D:latest-arm
+
+    # push the manifest that won the battle
+    docker manifest push ${ACCOUNT:-opensourcefoundries}/$D:latest
+
+    echo "Build Completed, multiarch images found for: "
+    docker manifest inspect ${ACCOUNT:-opensourcefoundries}/$D:latest | grep architecture
+
+}
+# build docker compose with the local yml files
+docker build -f docker-compose/Dockerfile -t ${ACCOUNT:-opensourcefoundries}/docker-compose:latest$arch .
+docker push ${ACCOUNT:-opensourcefoundries}/docker-compose:latest$arch
+create_and_push_manifest ${ACCOUNT:-opensourcefoundries} "docker-compose"
+
 for D in simple*
 do
     pushd $D
     echo
     docker build -t ${ACCOUNT:-opensourcefoundries}/$D:latest$arch --force-rm .
     docker push ${ACCOUNT:-opensourcefoundries}/$D:latest$arch
+
+    create_and_push_manifest ${ACCOUNT:-opensourcefoundries} $D
+
     popd
 done
-
-# create a manifest for atleast 1 image
-docker manifest create --amend \
-    ${ACCOUNT:-opensourcefoundries}/$D:latest \
-        ${ACCOUNT:-opensourcefoundries}/$D:latest$arch
-
-# create a manifest for atleast 2 images
-docker manifest create --amend \
-    ${ACCOUNT:-opensourcefoundries}/$D:latest \
-        ${ACCOUNT:-opensourcefoundries}/$D:latest-arm64 \
-        ${ACCOUNT:-opensourcefoundries}/$D:latest-amd64
-docker manifest create --amend \
-    ${ACCOUNT:-opensourcefoundries}/$D:latest \
-        ${ACCOUNT:-opensourcefoundries}/$D:latest-arm64 \
-        ${ACCOUNT:-opensourcefoundries}/$D:latest-arm
-docker manifest create --amend \
-    ${ACCOUNT:-opensourcefoundries}/$D:latest \
-        ${ACCOUNT:-opensourcefoundries}/$D:latest-arm64 \
-        ${ACCOUNT:-opensourcefoundries}/$D:latest-amd64
-
-# create a manifest for atleast 2 images
-docker manifest create --amend \
-    ${ACCOUNT:-opensourcefoundries}/$D:latest \
-        ${ACCOUNT:-opensourcefoundries}/$D:latest-arm64 \
-        ${ACCOUNT:-opensourcefoundries}/$D:latest-amd64 \
-        ${ACCOUNT:-opensourcefoundries}/$D:latest-arm
-
-# push the manifest that won the battle
-docker manifest push ${ACCOUNT:-opensourcefoundries}/$D:latest
-
-echo "Build Completed, multiarch images found for: "
-docker manifest inspect ${ACCOUNT:-opensourcefoundries}/$D:latest | grep architecture
-
-#Once you build a docker file for each architecture, you can add a manifest
-#manifest-tool-darwin-amd64 \
-#        --username $USER --password $PASSWORD push from-args \
-#        --platforms linux/amd64,linux/arm64 \
-#        --template ${ACCOUNT:-opensourcefoundries}/$D:latest-ARCH \
-#        --target ${ACCOUNT:-opensourcefoundries}/$D:latest
